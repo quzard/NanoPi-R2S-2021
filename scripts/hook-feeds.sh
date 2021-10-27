08a3aff7d21c9cdf9b8a886b354468e2728a72fa
quzard <1191890118@qq.com>
2021-10-27 19:55:30 +0800

    update

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# luci-app-seu-net
+git clone https://github.com/quzard/luci-app-seu-net.git package/luci-app-seu-net
+
+# sub-web
+rm -rf $(find /home/user/NanoPi-R2S-2021/openwrt -name "*sub-web*")
+git clone https://github.com/quzard/openwrt-sub-web.git package/openwrt-sub-web
+
+
 # Svn checkout packages from immortalwrt's repository
 pushd customfeeds
 
@@ -34,14 +43,6 @@ svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/mi
 # Replace smartdns with the official version
 rm -rf packages/net/smartdns
 svn co https://github.com/openwrt/packages/trunk/net/smartdns packages/net/smartdns
-
-# luci-app-seu-net
-git clone https://github.com/quzard/luci-app-seu-net.git package/luci-app-seu-net
-
-# sub-web
-rm -rf $(find /home/user/NanoPi-R2S-2021/openwrt -name "*sub-web*")
-git clone https://github.com/quzard/openwrt-sub-web.git package/openwrt-sub-web
-
 popd
 
 # Set to local feeds
