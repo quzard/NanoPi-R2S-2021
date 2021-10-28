155d4a41986516e2ffa36f47f67a1fb6f5e4d337
quzard <1191890118@qq.com>
2021-10-28 11:32:05 +0800

    更改自定义config位置

--- scripts/modify_config.sh
+++ scripts/modify_config.sh
@@ -2,3 +2,19 @@ sed -i "s/CONFIG_TARGET_ARCH_PACKAGES=\"aarch64_cortex-a53\"/CONFIG_TARGET_ARCH_
 sed -i "s/CONFIG_DEFAULT_TARGET_OPTIMIZATION=\"-Os -pipe -mcpu=cortex-a53\"/CONFIG_DEFAULT_TARGET_OPTIMIZATION=\"-O3 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a72.cortex-a53+crypto+crc -mtune=cortex-a72.cortex-a53\"/" .config
 sed -i "s/CONFIG_CPU_TYPE=\"cortex-a53\"/CONFIG_CPU_TYPE=\"cortex-a72.cortex-a53\"/" .config
 sed -i "s/CONFIG_TARGET_OPTIMIZATION=\"-Os -pipe -mcpu=cortex-a53\"/CONFIG_TARGET_OPTIMIZATION=\"-O3 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a72.cortex-a53+crypto+crc -mtune=cortex-a72.cortex-a53\"/" .config
+
+echo "
+CONFIG_PACKAGE_openssh-sftp-server=y
+CONFIG_PACKAGE_iperf3=y
+CONFIG_PACKAGE_subconverter=y
+CONFIG_PACKAGE_sub-web=y
+CONFIG_PACKAGE_luci-app-seu-net=y
+CONFIG_PACKAGE_luci-app-frpc=y
+
+CONFIG_PACKAGE_luci-app-jd-dailybonus=y
+CONFIG_PACKAGE_luci-app-ttyd=y
+CONFIG_PACKAGE_luci-app-unblockmusic=y
+CONFIG_UnblockNeteaseMusic_Go=y
+CONFIG_UnblockNeteaseMusic_NodeJS=y
+CONFIG_PACKAGE_luci-app-usb-printer=y
+" >> .config
