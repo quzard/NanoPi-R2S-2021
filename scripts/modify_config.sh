867cdaa1a7da2435ca6effe89c0dc124f9c53ca6
quzard <1191890118@qq.com>
2021-11-05 23:43:23 +0800

    删除usb_printer

--- scripts/modify_config.sh
+++ scripts/modify_config.sh
@@ -14,5 +14,5 @@ CONFIG_PACKAGE_luci-app-ttyd=y
 CONFIG_PACKAGE_luci-app-unblockmusic=y
 CONFIG_UnblockNeteaseMusic_Go=y
 CONFIG_UnblockNeteaseMusic_NodeJS=y
-CONFIG_PACKAGE_luci-app-usb-printer=y
+
 " >> .config
